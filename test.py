5625937fae5ad0f547cb28af7861af197cf85684
sakaloner <andresf40y0@gmail.com>
2022-09-12 22:56:03 -0500

    tested a right way to get the correlation

--- test.py
+++ test.py
@@ -1,25 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
- 
-def estimate_coef(x, y):
-    # number of observations/points
-    x = np.array(x)
-    y = np.array(y)
-    n = np.size(x)
- 
-    # mean of x and y vector
-    m_x = np.mean(x)
-    m_y = np.mean(y)
- 
-    # calculating cross-deviation and deviation about x
-    SS_xy = np.sum(y*x) - n*m_y*m_x
-    SS_xx = np.sum(x*x) - n*m_x*m_x
- 
-    # calculating regression coefficients
-    b_1 = SS_xy / SS_xx
-    b_0 = m_y - b_1*m_x
-    
-    return (b_0, b_1)
+'''Hacer esto en cada paso y luego sacarle el mean'''
 
+from scipy.stats import pearsonr
 
-print(estimate_coef([797,477,446], [13,13,4]))
+print(pearsonr([797,477,446], [13,13,4]))
