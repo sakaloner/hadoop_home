600ee76fba5e580ed4a8fd26b3bfb97512e13ae7
sakaloner <andresf40y0@gmail.com>
2022-09-13 20:19:58 -0500

    nio

--- mapreduce.sh
+++ mapreduce.sh
@@ -1,5 +1,8 @@
 mapred streaming \
--input /hadoop_project/book.txt \  
--output /tmp/guten/analized.txt \
--mapper /home/andresv/hadoop_home/mapper.py \
--reducer /home/andresv/hadoop_home/reducer.py
+    -input '/home/andresv/hadoop_home/hadoop_project/112010 Stack Overflow/posts.xml' \  
+    -output /home/andresv/hadoop_home/analized.txt \
+    -mapper /home/andresv/hadoop_home/mapper2.py \
+    -reducer /home/andresv/hadoop_home/reducer2.py
+
+
+mapred streaming -input '/home/andresv/hadoop_home/hadoop_project/112010 Stack Overflow/posts.xml' -output /home/andresv/hadoop_home/analized.txt -mapper /home/andresv/hadoop_home/mapper2.py -reducer /home/andresv/hadoop_home/reducer2.py
